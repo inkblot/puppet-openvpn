5d1ff682bea996a8e0c8d05ea67558cace2274fb
Nate Riffe <inkblot@movealong.org>
2016-07-24 07:18:16 -0500

    Add content parameters for key material
    Allow use of either `*_source` or `*_content` parameters on the
    `openvpn::server` and `openvpn::client` types as requested in PR #2

--- manifests/client.pp
+++ manifests/client.pp
@@ -16,12 +16,27 @@ define openvpn::client (
     $mtu_discovery        = true,
     $connections,
     $ca_cert_source,
+    $ca_cert_content,
     $cert_source,
+    $cert_content,
     $key_source,
+    $key_content,
 ) {
     $easy_rsa = $::openvpn::easy_rsa_path
     $vpn_dir = "/etc/openvpn/${name}"
     $ssl_dir = "${vpn_dir}/ssl"
+
+    if (empty($ca_cert_source) and empty($ca_cert_content)) {
+        fail('Must specify either ca_cert_source or ca_cert_content property of openvpn::client')
+    }
+
+    if (empty($cert_source) and empty($cert_content)) {
+        fail('Must specify either cert_source or cert_content property of openvpn::client')
+    }
+
+    if (empty($key_source) and empty($key_content)) {
+        fail('Must specify either key_source or key_content property of openvpn::client')
+    }
 
     File {
         ensure => present,
@@ -41,18 +56,21 @@ define openvpn::client (
     }
 
     file { "${ssl_dir}/ca.crt":
-        mode   => '0644',
-        source => $ca_cert_source,
+        mode    => '0644',
+        source  => $ca_cert_source,
+        content => $ca_cert_content,
     }
 
     file { "${ssl_dir}/client.crt":
-        mode   => '0644',
-        source => $cert_source,
+        mode    => '0644',
+        source  => $cert_source,
+        content => $cert_content,
     }
 
     file { "${ssl_dir}/client.key":
-        mode   => '0400',
-        source => $key_source,
+        mode    => '0400',
+        source  => $key_source,
+        content => $key_content,
     }
 
     if $tls_auth_source {
